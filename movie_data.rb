e5d13f5e3713c582401927bdb739b7a1c326eebf
William Tarimo <wtarimo@brandeis.edu>
2014-01-28 12:40:19 -0500

    PA02 1st Complete Code

--- movie_data.rb
+++ movie_data.rb
@@ -2,72 +2,133 @@
 #COSI236B - PA02
 #1/23/2014
 
-class MovieData
-	attr_accessor :file, :movies, :users
 
-	def initialize(file_path)
-		if File.exists? file_path and File.file? file_path
-			@file = file_path
-			@movies = Hash.new { |hash, key| hash[key] = [0,0] } #hash[movie_id] = [totalRates, sumRates]
+require 'pathname'
+require_relative 'movie_test'
+
+
+class MovieData
+	attr_accessor :training_file, :test_file, :movieRatings, :users, :views
+
+	def initialize(path,pair=nil)
+		folder = Pathname.new(path)
+		if folder.directory?
+			@movieRatings = Hash.new { |hash, key| hash[key] = [0,0] } #hash[movie_id] = [totalRates, sumRates]
 			@users = Hash.new { |hash, key| hash[key] = [] } #hash[user_id] = [movie_id, rating]
+			@views = Hash.new { |hash, key| hash[key] = [] } #hash[movie_id] = [viewer_user_ids]
+		
+			@training_file = folder.join('u.data')
+			if pair
+				@training_file = folder.join("#{pair}.base")
+				@test_file = folder.join("#{pair}.test")
+			end
+			load_data(@training_file)
 		else
-			puts "#{file_path} doesn't exist or isn't a file!"
+			puts "#{path} isn't a valid directory"
 			exit
 		end
 	end
 
-	def load_data
-		#Reads data from @file and populates @movies and @users
-		File.open(@file,"r").each_line do |line|
+	def load_data(file)
+		#Reads data from file and populates @movieRatings, @viewers and @users
+		File.open(file,"r").each_line do |line|
 			entry = line.split
-			@movies[entry[1].to_sym][0] += 1
-			@movies[entry[1].to_sym][1] += entry[2].to_i
+			@movieRatings[entry[1].to_sym][0] += 1
+			@movieRatings[entry[1].to_sym][1] += entry[2].to_i
 			@users[entry[0].to_sym].push([entry[1],entry[2].to_i])
-		end
-	
-	end
-
-	def popularity(movie_id)
-		#Returns a rank out of all movie ids that indicates the popularity (higher number means more popular)
-		#Polularity is measured by sum of ratings
-		index = popularity_list.index movie_id.to_s.to_sym
-		if index
-			return @movies.keys.size - index
-		else
-			puts "Movie ID not database!"
-			return nil
+			@views[entry[1].to_sym].push(entry[0])
 		end
 	end
 
-	def popularity_list
-		#Generates and returns a list of all movie_id's ordered by decreasing popularity
-		#Movies are sorted by first the sum of ratings, then the number of ratings
-		movie_ids = @movies.keys
-		movie_ids.sort_by{|key| [@movies[key][1],@movies[key][0]]}.reverse
-	
+	def rating(u,m)
+		#Returns the rating that user u gave movie m in the training set, 
+		#and 0 if user u did not rate movie m
+		raise "User not in Database" unless @users.has_key? u.to_s.to_sym
+		rate = 0
+		@users[u.to_s.to_sym].each {|id,rating| return rating if id==m.to_s}
+		return rate
+
 	end
+
+	def predict(u,m)
+		#returns a floating point number between 1.0 and 5.0 as an estimate of what user u would rate movie m
+		viewedUsers = viewers(m).drop(1)
+		return 3.0 unless viewedUsers.size > 0
+		#print "viewedUsers: ",viewedUsers
+		wRatings = []
+		similarities = []
+		viewedUsers.each {|user| similarities.push similarity(u,user)}
+		#print "similarities: ",similarities
+		#puts
+		min = 0
+		min = similarities.min if similarities.size>1
+		scale = (similarities.max-min)/1.5
+		viewedUsers.each {|user| wRatings.push(rating(user,m)*(similarity(u,user)-min).to_f/scale)}
+		#print "wRatings: ",wRatings
+		#puts
+		scale = (wRatings.max - wRatings.min)/4.0
+		wRatings2=[]
+		wRatings.each {|rating| wRatings2.push(1.0+(rating-wRatings.min)/scale)}
+		#print "wRatings2: ",wRatings2
+		return wRatings2.instance_eval { reduce(:+) / size.to_f }
+	end
+
+	def movies(u)
+		#returns the array of movies that user u has watched
+		raise "User not in Database" unless @users.has_key? u.to_s.to_sym
+		movies = []
+		@users[u.to_s.to_sym].each {|id,rating| movies.push id}
+		return movies
+
+	end
+
+	def viewers(m)
+		#returns the array of users that have seen movie m
+		@views[m.to_s.to_sym]
+	end
+
+	def test(k=nil)
+		#runs the z.predict method on the first k ratings in the test set and returns a MovieTest object containing the results.
+		#The parameter k is optional and if omitted, all of the tests will be run.
+		raise 'Test Set not defined!' if @test_file.nil?
+		predictions = []
+		count = 0
+		File.open(@test_file,"r").each_line do |line|
+			entry = line.split
+			entry[3] = predict(entry[0],entry[1])
+			predictions.push(entry)
+			count+=1
+			break if (k and count >= k)
+		end
+		return MovieTest.new predictions
+	end
+
+
 
 	def similarity(user1,user2)
 		#Generates a number which indicates the similarity in movie preference btn users 1 and 2
 		#Higher numbers indicates greater similarity
+		similarity = 0
 		if @users.has_key? user1.to_s.to_sym and @users.has_key? user2.to_s.to_sym
-			similarity = 0
+			
 			ratings1 = @users[user1.to_s.to_sym]
 			ratings2 = @users[user2.to_s.to_sym]
 			ids2 = []
 			ratings2.each {|id,rating| ids2.push id}
 			ratings1.each {|id,rating| similarity += 5 -(rating - ratings2[ids2.index id][1]).abs if ids2.include? id}
-			return similarity
 		else
 			puts "User(s) not in database"
-			return nil
 		end
-	end
-
-	def most_similar(u)
-		#Returns a list of users whose tastes are most similar to the tastes of user u
-		user_ids = @users.keys
-		user_ids.sort_by {|id| [similarity(u,id)]}.reverse.drop(1) #The 1st is always u
+		return similarity
 	end
 
 end
+
+
+
+z = MovieData.new('ml-100k',:u1)
+t = z.test()
+puts t.mean
+puts t.stddev
+puts t.rms
+#print t.to_a
--- movie_test.rb
+++ movie_test.rb
@@ -3,71 +3,43 @@
 #1/23/2014
 
 class MovieTest
-	attr_accessor :file, :movies, :users
+	attr_accessor :predictions, :avgError
 
-	def initialize(file_path)
-		if File.exists? file_path and File.file? file_path
-			@file = file_path
-			@movies = Hash.new { |hash, key| hash[key] = [0,0] } #hash[movie_id] = [totalRates, sumRates]
-			@users = Hash.new { |hash, key| hash[key] = [] } #hash[user_id] = [movie_id, rating]
-		else
-			puts "#{file_path} doesn't exist or isn't a file!"
-			exit
+	def initialize(predictions)
+		@predictions = predictions
+		totalError = 0
+		predictions.each do |p|
+			totalError+=(p[2].to_f-p[3]).abs
 		end
+		@avgError = totalError/predictions.size.to_f
 	end
 
-	def load_data
-		#Reads data from @file and populates @movies and @users
-		File.open(@file,"r").each_line do |line|
-			entry = line.split
-			@movies[entry[1].to_sym][0] += 1
-			@movies[entry[1].to_sym][1] += entry[2].to_i
-			@users[entry[0].to_sym].push([entry[1],entry[2].to_i])
+	def mean
+		#Returns the average predication error (which should be close to zero)
+		return @avgError
+	end
+
+	def stddev
+		#Returns the standard deviation of the error
+		dev = 0
+		@predictions.each do |p|
+			dev += ((p[2].to_f-p[3]).abs-@avgError)**2
 		end
-	
+		return Math.sqrt(dev/@predictions.size)
+		
 	end
 
-	def popularity(movie_id)
-		#Returns a rank out of all movie ids that indicates the popularity (higher number means more popular)
-		#Polularity is measured by sum of ratings
-		index = popularity_list.index movie_id.to_s.to_sym
-		if index
-			return @movies.keys.size - index
-		else
-			puts "Movie ID not database!"
-			return nil
+	def rms
+		#Returns the root mean square error of the prediction
+		eSquared = 0
+		@predictions.each do |p|
+			eSquared += (p[2].to_f-p[3])**2
 		end
+		return Math.sqrt(eSquared/@predictions.size)
 	end
 
-	def popularity_list
-		#Generates and returns a list of all movie_id's ordered by decreasing popularity
-		#Movies are sorted by first the sum of ratings, then the number of ratings
-		movie_ids = @movies.keys
-		movie_ids.sort_by{|key| [@movies[key][1],@movies[key][0]]}.reverse
-	
+	def to_a
+		#Returns an array of the predictions in the form [u,m,r,p]. 
+		return @predictions
 	end
-
-	def similarity(user1,user2)
-		#Generates a number which indicates the similarity in movie preference btn users 1 and 2
-		#Higher numbers indicates greater similarity
-		if @users.has_key? user1.to_s.to_sym and @users.has_key? user2.to_s.to_sym
-			similarity = 0
-			ratings1 = @users[user1.to_s.to_sym]
-			ratings2 = @users[user2.to_s.to_sym]
-			ids2 = []
-			ratings2.each {|id,rating| ids2.push id}
-			ratings1.each {|id,rating| similarity += 5 -(rating - ratings2[ids2.index id][1]).abs if ids2.include? id}
-			return similarity
-		else
-			puts "User(s) not in database"
-			return nil
-		end
-	end
-
-	def most_similar(u)
-		#Returns a list of users whose tastes are most similar to the tastes of user u
-		user_ids = @users.keys
-		user_ids.sort_by {|id| [similarity(u,id)]}.reverse.drop(1) #The 1st is always u
-	end
-
 end
